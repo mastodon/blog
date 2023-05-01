eae3751a153eeb1c55a82d1ac8190161a7ebacde
Eugen Rochko <eugen@zeonfederated.com>
2023-05-01 15:14:45 +0200

    Change font to Manrope

--- themes/mastodon/tailwind.config.js
+++ themes/mastodon/tailwind.config.js
@@ -1,3 +1,6 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./themes/**/layouts/**/*.html",
@@ -6,7 +9,40 @@ module.exports = {
     "./content/**/*.html"
   ],
   theme: {
-    extend: {},
+    colors: {
+      ...colors,
+
+      blurple: {
+        300: "#858afa",
+        500: "#6364ff",
+        600: "#563acc", // "main"
+        900: "#2f0c7a",
+      },
+    },
+
+    extend: {
+      fontFamily: {
+        sans: ["Manrope", ...defaultTheme.fontFamily.sans],
+      },
+
+      backgroundImage: {
+        "blurple-gradient": `linear-gradient(0deg, #563acc 12.87%, #6364ff 88.62%)`,
+      },
+
+      typography: {
+        DEFAULT: {
+          css: {
+            a: {
+              color: '#563acc',
+
+              '&:hover': {
+                color: '#6364ff',
+              },
+            },
+          },
+        },
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
